140c5dc822b84206f1096240ad36fcb36b9bd728
Yuri Kozhukhov <tweenitwits@gmail.com>
2025-09-08 19:15:59 +0300

    set up logger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,20 +1,50 @@
 package main
 
 import (
+	"log/slog"
+	"os"
+
 	"github.com/Tweenitwits/easy_rest_api_service.git/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 
 	cfg := config.MustLoad()
 
-	// TODO: init config: cleanenv
+	log := setupLogger(cfg.Env)
 
-	//TODO: init logger: slog
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
 
 	//TODO: init storage: sqlite
 
 	//TODO: init router: chi, "chi render"
 
 	//TODO: run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
 }
